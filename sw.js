620cc697fa0e1acca00044759702c0e63f935014
DanielPastorMiguel <96420014+DanielPastorMiguel@users.noreply.github.com>
2026-08-19 22:45:42 +0200

    v01.01
    Recetas modo tabla manual

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker de "Alimentación"
 // Sube este número cada vez que publiques cambios para forzar la actualización
 // del caché en los dispositivos de los usuarios.
-const CACHE_VERSION = 'v15';
+const CACHE_VERSION = 'v16';
 const CACHE_NAME = `alimentacion-${CACHE_VERSION}`;
 
 const APP_SHELL = [
